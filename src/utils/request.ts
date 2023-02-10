0353dbbf5f251fa4a26149cf127fa801e9ea0fba
lh <532622806@qq.com>
2023-02-10 10:28:41 +0800

    暂时使用老接口2

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const service = axios.create({
-    baseURL: 'http://localhost:8088',
+    baseURL: 'http://wzqyszl.com:8080',
     timeout: 30 * 1000,
     headers: {
         // 'Content-Type': 'application/json;charset=UTF-8',
